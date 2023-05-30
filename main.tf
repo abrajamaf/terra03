fba45609b206cacc49f61c778788be5224293578
Abraham Alvarado Fuentes <abrajam.alvarado@gmail.com>
2023-05-30 00:27:56 -0600

    Se cambia vpc en subnets

--- main.tf
+++ main.tf
@@ -28,21 +28,21 @@ resource "huaweicloud_vpc_subnet" "subnet1" {
   name       = "terra1-web"
   cidr       = "172.24.10.0/24"
   gateway_ip = var.terra1_gateway_ip
-  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 resource "huaweicloud_vpc_subnet" "subnet2" {
   name       = "terra1-app"
   cidr       = "172.24.11.0/24"
   gateway_ip = var.terra1_gateway_ip
-  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 resource "huaweicloud_vpc_subnet" "subnet3" {
   name       = "terra2-db"
   cidr       = "172.24.12.0/24"
   gateway_ip = var.terra1_gateway_ip
-  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 
