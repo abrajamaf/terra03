da9425b40b6c0c69932175317086550e8264602c
Abraham Alvarado Fuentes <abrajam.alvarado@gmail.com>
2023-05-30 00:08:12 -0600

    Crea VPC y subredes  main.tf

--- main.tf
+++ main.tf
@@ -5,36 +5,44 @@ variable "vpc_terra1" {
   default = "terra1_vpc"
 }
 
-variable "vpc_terra2" {
-  default = "terra2_vpc"
-}
-
 # CIDR de los VPCs
 variable "vpc_cidr_terra1" {
   default = "172.24.0.0/16"
 }
 
-variable "vpc_cidr_terra2" {
-  default = "172.25.0.0/16"
+variable "terra1_gateway_ip" {
+  default = "172.24.10.1"
 }
 
-# crea los VPCs
-resource "huaweicloud_vpc" "vpc_terra1" {
-  name = var.vpc_terra1
-  cidr = var.vpc_cidr_terra1
-}
-
-resource "huaweicloud_vpc" "vpc_terra2" {
-  name = var.vpc_terra2
-  cidr = var.vpc_cidr_terra2
-}
 
 resource "huaweicloud_vpc" "vpc_with_tags" {
   name = var.vpc_terra1
   cidr = var.vpc_cidr_terra1
-
   tags = {
     Ambiente = "DEV"
-    Otrigen = "terraform"
+    Otrigen  = "terraform"
   }
+
+
+resource "huaweicloud_vpc_subnet" "subnet1" {
+  name       = "terra1-web"
+  cidr       = "172.24.10.0/24"
+  gateway_ip = var.terra1_gateway_ip
+  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
+resource "huaweicloud_vpc_subnet" "subnet2" {
+  name       = "terra1-app"
+  cidr       = "172.24.11.0/24"
+  gateway_ip = var.terra1_gateway_ip
+  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  # dns_list   = ["100.125.1.250", "100.125.129.250"]
+}
+resource "huaweicloud_vpc_subnet" "subnet3" {
+  name       = "terra2-db"
+  cidr       = "172.24.12.0/24"
+  gateway_ip = var.terra1_gateway_ip
+  vpc_id     = huaweicloud_vpc.vpc_terra1.id
+  # dns_list   = ["100.125.1.250", "100.125.129.250"]
+}
+
