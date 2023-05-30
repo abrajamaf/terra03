2a8b158f6e04fcf4ecf78fa703287d9216f51fd0
Abraham Alvarado Fuentes <abrajam.alvarado@gmail.com>
2023-05-30 01:33:29 -0600

    Mexico zonez_mexico_2_names 2

--- main.tf
+++ main.tf
@@ -14,6 +14,13 @@ variable "terra1_gateway_ip" {
   default = "172.24.10.1"
 }
 
+# Data sources 
+data "huaweicloud_availability_zones" "mexico_2" {
+  region = "la-north-2"
+  state  = "available"
+}
+
+
 
 resource "huaweicloud_vpc" "vpc_with_tags" {
   name = var.vpc_terra1
@@ -25,24 +32,30 @@ resource "huaweicloud_vpc" "vpc_with_tags" {
 }
 
 resource "huaweicloud_vpc_subnet" "subnet1" {
-  name       = "terra1-web"
-  cidr       = "172.24.10.0/24"
-  gateway_ip = var.terra1_gateway_ip
-  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
+  name        = "terra1-web"
+  cidr        = "172.24.10.0/24"
+  gateway_ip  = var.terra1_gateway_ip
+  ipv6_enable = false
+  availability_zone  = data.huaweicloud_availability_zones.mexico_2.names.[0]
+  vpc_id      = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 resource "huaweicloud_vpc_subnet" "subnet2" {
-  name       = "terra1-app"
-  cidr       = "172.24.11.0/24"
-  gateway_ip = "172.24.11.1"
-  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
+  name        = "terra1-app"
+  cidr        = "172.24.11.0/24"
+  gateway_ip  = "172.24.11.1"
+  ipv6_enable = false
+  availability_zone  = data.huaweicloud_availability_zones.mexico_2.names.[1]
+  vpc_id      = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 resource "huaweicloud_vpc_subnet" "subnet3" {
-  name       = "terra2-db"
-  cidr       = "172.24.12.0/24"
-  gateway_ip = "172.24.12.1"
-  vpc_id     = huaweicloud_vpc.vpc_with_tags.id
+  name        = "terra2-db"
+  cidr        = "172.24.12.0/24"
+  gateway_ip  = "172.24.12.1"
+  ipv6_enable = false
+  vpc_id      = huaweicloud_vpc.vpc_with_tags.id
+  availability_zone  = data.huaweicloud_availability_zones.mexico_2.names.[0]
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 
