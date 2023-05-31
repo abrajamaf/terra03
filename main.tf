56932a7c8f95927551d27cc237f495262412a7ab
Abraham Alvarado Fuentes <abrajam.alvarado@gmail.com>
2023-05-30 21:25:21 -0600

    despliegue de ecs_terra01 y 02

--- main.tf
+++ main.tf
@@ -10,14 +10,22 @@ variable "vpc_cidr_terra1" {
   default = "172.24.0.0/16"
 }
 
-variable "terra1_gateway_ip" {
-  default = "172.24.10.1"
-}
-
 # Data sources 
 data "huaweicloud_availability_zones" "mexico_2" {
   region = "la-north-2"
   state  = "available"
+}
+data "huaweicloud_compute_flavors" "myflavor" {
+  availability_zone = data.huaweicloud_availability_zones.mexico_2.names[0]
+  performance_type  = "normal"
+  cpu_core_count    = 1
+  memory_size       = 2
+}
+
+data "huaweicloud_images_image" "myimage" {
+  os          = "Ubuntu"
+  visibility  = "public"
+  most_recent = true
 }
 
 
@@ -31,17 +39,18 @@ resource "huaweicloud_vpc" "vpc_with_tags" {
   }
 }
 
+# Sunredes de terra1
 resource "huaweicloud_vpc_subnet" "subnet1" {
-  name              = "terra1-web"
+  name              = "terra1-az1"
   cidr              = "172.24.10.0/24"
-  gateway_ip        = var.terra1_gateway_ip
+  gateway_ip        = "172.24.10.1"
   ipv6_enable       = false
   availability_zone = data.huaweicloud_availability_zones.mexico_2.names[0]
   vpc_id            = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
 resource "huaweicloud_vpc_subnet" "subnet2" {
-  name              = "terra1-app"
+  name              = "terra1-az3"
   cidr              = "172.24.11.0/24"
   gateway_ip        = "172.24.11.1"
   ipv6_enable       = false
@@ -49,13 +58,33 @@ resource "huaweicloud_vpc_subnet" "subnet2" {
   vpc_id            = huaweicloud_vpc.vpc_with_tags.id
   # dns_list   = ["100.125.1.250", "100.125.129.250"]
 }
-resource "huaweicloud_vpc_subnet" "subnet3" {
-  name              = "terra2-db"
-  cidr              = "172.24.12.0/24"
-  gateway_ip        = "172.24.12.1"
-  ipv6_enable       = false
-  vpc_id            = huaweicloud_vpc.vpc_with_tags.id
-  availability_zone = data.huaweicloud_availability_zones.mexico_2.names[0]
-  # dns_list   = ["100.125.1.250", "100.125.129.250"]
+
+# Instancias ECS
+variable "secgroup_id" {}
+
+
+resource "huaweicloud_compute_instance" "ecs_basic01" {
+  name               = "ecs_terra01"
+  image_id           = data.huaweicloud_images_image.myimage.id
+  flavor_id          = data.huaweicloud_compute_flavors.myflavor.id
+  # security_group_ids = [var.secgoup_id]
+  # availability_zone  = data.huaweicloud_availability_zones.mexico_2.names[0]
+
+  network {
+    uuid = data.huaweicloud_vpc_subnet.subnet1.id
+  }
 }
+
+resource "huaweicloud_compute_instance" "ecs_basic02" {
+  name               = "ecs_terra02"
+  image_id           = data.huaweicloud_images_image.myimage.id
+  flavor_id          = data.huaweicloud_compute_flavors.myflavor.id
+  # security_group_ids = [var.secgroup_id]
+  # availability_zone  = data.huaweicloud_availability_zones.mexico_2.names[1]
+
+  network {
+    uuid = data.huaweicloud_vpc_subnet.subnet2.id
+  }
+}
+
 
